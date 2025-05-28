a53801ffce22ec059e758b59bc9fe0eaca242e07
Anubhav Kumar <151345311+anubhavkumar47@users.noreply.github.com>
2025-05-28 15:37:11 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,29 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class MLPActor(nn.Module):
-    def __init__(self, state_dim, action_dim, max_action, hidden_dim=400, dropout_p=0.2):
-        super(MLPActor, self).__init__()
-        self.max_action = max_action
-
-        # Input to first hidden layer
-        self.fc1 = nn.Sequential(
-            nn.Linear(state_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Linear(hidden_dim, hidden_dim//2),
-            nn.Tanh(),
-            nn.Linear(hidden_dim//2, action_dim),
-            nn.Tanh()
-
-        )
-
-
-    def forward(self, state):
-        action = self.fc1(state)
-
-        return action * self.max_action
-
-
 class DoubleCritic(nn.Module):
     def __init__(self, state_dim, action_dim):
         super(DoubleCritic, self).__init__()
